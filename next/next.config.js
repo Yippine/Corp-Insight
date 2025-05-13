991c0d23aae890f8b89c097e6ca2676713f4727c
Yippine <Olange.stone@gmail.com>
2025-05-13 14:11:09 +0800

    [chore] Set the Production Environment Port Number to 4173 | Fix Netlify deployment settings.

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   // 禁用靜態優化以解決動態 API 路由問題
   staticPageGenerationTimeout: 180,
   // 禁用生成靜態頁面導出，解決 API 路由中使用 searchParams 的問題
