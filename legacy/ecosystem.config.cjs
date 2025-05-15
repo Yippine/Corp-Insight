7ae00ec730d436b0a0bace6739498b8cfc615305
Yippine <Olange.stone@gmail.com>
2025-05-15 13:54:49 +0800

    [chore] Change the original project path to v1 to accommodate the v2 project for the next branch.

--- legacy/ecosystem.config.cjs
+++ legacy/ecosystem.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [{
-    name: 'business-magnifier',
+    name: 'business-magnifier-v1',
     script: 'npm',
     args: 'run preview',
-    cwd: '/home/ec2-user/Business-Magnifier',
+    cwd: '/home/ec2-user/Business-Magnifier-v1',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -13,9 +13,9 @@ module.exports = {
       PORT: 4173,
       HOST: '0.0.0.0'
     },
-    log_file: '/var/log/pm2/business-magnifier.log',
-    error_file: '/var/log/pm2/business-magnifier-error.log',
-    out_file: '/var/log/pm2/business-magnifier-out.log',
+    log_file: '/var/log/pm2/business-magnifier-v1.log',
+    error_file: '/var/log/pm2/business-magnifier-error-v1.log',
+    out_file: '/var/log/pm2/business-magnifier-out-v1.log',
     merge_logs: true,
     time: true
   }]
