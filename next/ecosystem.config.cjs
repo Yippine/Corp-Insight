ace763586174629bf59ced3786d942e074224c40
Yippine <Olange.stone@gmail.com>
2025-05-15 15:55:44 +0800

    [chore] Configure the ecosystem.prod.config.cjs for the next branch.

--- next/ecosystem.config.cjs
+++ next/ecosystem.config.cjs
@@ -1,22 +1,21 @@
 module.exports = {
   apps: [{
-    name: 'business-magnifier-next',
+    name: 'business-magnifier-v2',
     script: 'npm',
     args: 'run dev',
-    cwd: './',
+    cwd: '/home/ec2-user/Business-Magnifier-v2/next',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development',
-      PORT: 4173,
-      HOST: '0.0.0.0',
-      NEXT_PUBLIC_SKIP_BUILD_STATIC_GENERATION: 'true'
+      PORT: 3000,
+      HOST: '0.0.0.0'
     },
-    log_file: '/var/log/pm2/business-magnifier-next.log',
-    error_file: '/var/log/pm2/business-magnifier-next-error.log',
-    out_file: '/var/log/pm2/business-magnifier-next-out.log',
+    log_file: '/var/log/pm2/business-magnifier-v2.log',
+    error_file: '/var/log/pm2/business-magnifier-error-v2.log',
+    out_file: '/var/log/pm2/business-magnifier-out-v2.log',
     merge_logs: true,
     time: true
   }]
