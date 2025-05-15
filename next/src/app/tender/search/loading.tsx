41723593679b3f7a00c2b94666db581064ce2012
Yippine <Olange.stone@gmail.com>
2025-05-15 09:34:08 +0800

    [front-end] Create a Loading Component for the Tender Search Page

---
+++ next/src/app/tender/search/loading.tsx
@@ -0,0 +1,9 @@
+import { InlineLoading } from '@/components/common/loading/LoadingTypes';
+
+export default function TenderSearchLoading() {
+  return (
+    <div className="min-h-[50vh] w-full flex items-center justify-center">
+      <InlineLoading />
+    </div>
+  );
+}
