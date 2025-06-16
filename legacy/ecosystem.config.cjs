b8629ce7e2559ff419a21e9d4422edb705bab5a8
Yippine <Olange.stone@gmail.com>
2025-06-16 14:36:51 +0800

    [deploy] Update ecosystem configuration files.

--- legacy/ecosystem.config.cjs
+++ legacy/ecosystem.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [{
-    name: 'business-magnifier-v1',
+    name: 'business-magnifier-legacy',
     script: 'npm',
     args: 'run preview',
-    cwd: '/home/ec2-user/Business-Magnifier-v1',
+    cwd: '/home/ec2-user/Business-Magnifier/legacy',
     instances: 1,
     autorestart: true,
     watch: false,
@@ -13,9 +13,9 @@ module.exports = {
       PORT: 4173,
       HOST: '0.0.0.0'
     },
-    log_file: '/var/log/pm2/business-magnifier-v1.log',
-    error_file: '/var/log/pm2/business-magnifier-error-v1.log',
-    out_file: '/var/log/pm2/business-magnifier-out-v1.log',
+    log_file: '/var/log/pm2/business-magnifier-legacy.log',
+    error_file: '/var/log/pm2/business-magnifier-error-legacy.log',
+    out_file: '/var/log/pm2/business-magnifier-out-legacy.log',
     merge_logs: true,
     time: true
   }]
--- next/ecosystem.config.cjs
+++ next/ecosystem.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'business-magnifier-v2',
+      name: 'business-magnifier-next',
       script: 'npm',
       args: 'run dev',
-      cwd: '/home/ec2-user/Business-Magnifier-v2/next',
+      cwd: '/home/ec2-user/Business-Magnifier/next',
       instances: 1,
       autorestart: true,
       watch: false,
@@ -14,9 +14,9 @@ module.exports = {
         PORT: 3000,
         HOST: '0.0.0.0',
       },
-      log_file: '/var/log/pm2/business-magnifier-v2.log',
-      error_file: '/var/log/pm2/business-magnifier-error-v2.log',
-      out_file: '/var/log/pm2/business-magnifier-out-v2.log',
+      log_file: '/var/log/pm2/business-magnifier-next.log',
+      error_file: '/var/log/pm2/business-magnifier-error-next.log',
+      out_file: '/var/log/pm2/business-magnifier-out-next.log',
       merge_logs: true,
       time: true,
     },
